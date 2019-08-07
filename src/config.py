a1922cdbf2dcb5310f08b13711a485d6a392c91e
Stanislav Gelman <glmnio@outlook.com>
2019-08-08 02:55:53 +0300

    make empty

--- src/config.py
+++ src/config.py
@@ -4,8 +4,8 @@ config = {
     'no_matches': 1200.0
   },
   'tokens': {
-    'discord': 'NDg1MjE0MDg4NzYzNTM5NDY2.XUsqAw.AeKXrFcp_SA-lhpIFvPileQe0t8',
-    'pubg': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5Zjk5YTA0MC04MDI1LTAxMzctYmMyOS00MWRhZDE1OWU5NDIiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTYyMjAyMjc5LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InNwZWVkc3Rlci1nZ2ctIn0.VKB-CCGzg9uJDCti06wshILSSR72NUR8siPWu6Pp5Hg'
+    'discord': 'DISCORD TOKEN HERE',
+    'pubg': 'PUBG TOKEN HERE'
   },
   'database': {
     'path': 'db.json'
