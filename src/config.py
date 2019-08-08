6149cbd1b8f99716d9c5e50a72e28712f7628612
Stanislav Gelman <glmnio@outlook.com>
2019-08-08 03:03:51 +0300

    add files

---
+++ src/config.py
@@ -0,0 +1,18 @@
+config = {
+  'delay': {
+    'simple': 600.0,
+    'no_matches': 1200.0
+  },
+  'tokens': {
+    'discord': 'DISCORD TOKEN HERE',
+    'pubg': 'PUBG TOKEN HERE'
+  },
+  'database': {
+    'path': 'db.json'
+  },
+  'discord': {
+    'activity': {
+      'name': 'cOdInG...'
+    }
+  }
+}
