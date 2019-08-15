b116bf8ff21e80f41d935e1fa89f4211437304bb
Stanislav Gelman <glmnio@outlook.com>
2019-08-16 00:24:59 +0300

    fill tokens fiels

---
+++ src/config.py
@@ -0,0 +1,23 @@
+config = {
+  'bot': {
+    'rank_limit': 100,
+    'track_only_one': True,
+    'delete_delay': 10
+  },
+  'delay': {
+    'simple': 600.0,
+    'no_matches': 1200.0
+  },
+  'tokens': {
+    'discord': 'YOUR TOKEN',
+    'pubg': 'YOUR TOKEN'
+  },
+  'database': {
+    'path': 'db.json'
+  },
+  'discord': {
+    'activity': {
+      'name': 'БДСМ (ЖЕСТКААА!)'
+    }
+  }
+}
