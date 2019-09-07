6275301817b71fd584fab2f4e75ff83e26ba8493
Stanislav Gelman <glmnio@outlook.com>
2019-09-08 00:41:39 +0300

    :arrow_up: new ratelimiter from new pubg_python

--- src/pubg.py
+++ src/pubg.py
@@ -1,14 +1,22 @@
+import asyncio
 from config import config
+from datetime import datetime
 import pubg_python.exceptions
-from ratelimiter import rate_limiter
 from pubg_python import PUBG, Shard
 
 
 class pubg_manager:
 
     def __init__(self):
-        self.rate_limiter = rate_limiter(10.0, 60.0)
         self.api = PUBG(config['tokens']['pubg'], Shard.STEAM)
+
+    async def wait_ratelimit(self, reset):
+        sleep_seconds = (reset - datetime.now()).total_seconds()
+        print(sleep_seconds)
+        if sleep_seconds > 0:
+            return await asyncio.sleep(sleep_seconds)
+        else:
+            return True
 
     async def get_players_data(self, player_ids):
         players_chunks = list(self.chunk(player_ids, 10))
@@ -19,14 +27,16 @@ class pubg_manager:
                 print(e)
 
     async def get_players(self, player_ids):
-        await self.rate_limiter.wait()
-        return self.api.players().filter(player_ids=player_ids)
+        try:
+            return self.api.players().filter(player_ids=player_ids)
+        except pubg_python.exceptions.RateLimitError as e:
+            await self.wait_ratelimit(e.rl_reset)
+            return await self.get_players(player_ids)
 
     async def get_match(self, match_id):
         return self.api.matches().get(match_id)
 
     async def get_player_id_by_name(self, player_name):
-        await self.rate_limiter.wait()
         try:
             player = self.api.players().filter(
                 player_names=[player_name])[0]
@@ -36,8 +46,9 @@ class pubg_manager:
         except pubg_python.exceptions.NotFoundError:
             print('NotFoundError')
             return -1
-        except pubg_python.exceptions.RateLimitError:
+        except pubg_python.exceptions.RateLimitError as e:
             print('RateLimitError')
+            await self.wait_ratelimit(e.rl_reset)
             return await self.get_player_id_by_name(player_name)
 
     def find_roster_by_name(self, name, rosters):
