49f62b6565a63381094f4a3c61eb2f72653e21e0
Stanislav Gelman <glmnio@outlook.com>
2019-10-07 08:28:31 +0300

    :sparkles: add reload command for owner

---
+++ pubgdiscobot/cogs/reload.py
@@ -0,0 +1,27 @@
+from discord.ext import commands
+from discord.ext.commands import Cog
+
+MSG_PLAYER_FOUND = '{}, your ingame nickname is {}'
+MSG_NOT_REGISTERED = ''.join([
+    '{}, you have to register at first. ',
+    'Type `pubg reg IGN`, where IGN - your ingame nickname'
+])
+
+
+class ReloadCommand(Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='reload')
+    @commands.is_owner()
+    async def reload_command(self, ctx, ext):
+        try:
+            self.bot.reload_extension(f'pubgdiscobot.cogs.{ext}')
+            await ctx.send(f'Extension [{ext}] reloaded')
+        except:
+            await ctx.send(f'Error while reloading extension [{ext}]')
+
+
+def setup(bot):
+    bot.add_cog(ReloadCommand(bot))
